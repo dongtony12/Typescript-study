49a809a0089b8550896c56afd4aed939aefb9b05
김동현 <kimdonghyeon@gimdonghyeon-ui-MacBookPro.local>
2022-07-15 13:03:15 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [test, setTest] = useState("qr");
+  const targetRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (targetRef.current) console.log(targetRef.current.offsetHeight);
+  }, [targetRef]);
+
+  return <div ref={targetRef}>hello</div>;
+};
 
 export default App;
